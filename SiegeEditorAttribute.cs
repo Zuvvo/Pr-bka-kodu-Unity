10a6fa5cd42672a7721ba76d77d4d9028206d531
Mateusz Jarszak <Zuvvo4@gmail.com>
2025-03-24 17:45:25 +0100

    Add files via upload

---
+++ SiegeEditorAttribute.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorRangeAttribute : Attribute //implemented
+{
+    public float RangeMin { get; private set; }
+    public float RangeMax { get; private set; }
+    public bool IsDisplayedAsString { get; private set; }
+
+    public SiegeEditorRangeAttribute(float min, float max, bool isDisplayedAsString = false)
+    {
+        RangeMin = min;
+        RangeMax = max;
+        IsDisplayedAsString = isDisplayedAsString;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorSliderAttribute : Attribute //implemented
+{
+    public int RangeMin { get; private set; }
+    public int RangeMax { get; private set; }
+    public int Granulation { get; private set; }
+    public bool AddPercentSymbol { get; private set; }
+
+    public SiegeEditorSliderAttribute(int min = 0, int max = 100, int granulation = 1, bool addPercentSymbol = false)
+    {
+        RangeMin = min;
+        RangeMax = max;
+        Granulation = granulation;
+        AddPercentSymbol = addPercentSymbol;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorOnlyPositiveNumbersAttribute : Attribute
+{
+
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorOnlyNegativeNumbersAttribute : Attribute
+{
+
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorExcludeAttribute : Attribute
+{
+
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorTagNameFieldAttribute : Attribute
+{
+    public string Name { get { return tag_.Translate(); } private set { tag_ = value; } }
+    private string tag_;
+
+    public SiegeEditorTagNameFieldAttribute(string tag)
+    {
+        tag_ = tag;
+    }
+}
+
+public class SiegeEditorGroupAttribute : Attribute
+{
+    public string[] Groups { get; private set; }
+
+    public SiegeEditorGroupAttribute(params string[] groupNames)
+    {
+        Groups = groupNames;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorCheckboxAttribute : Attribute
+{
+    public SiegeEditorCheckboxAttribute()
+    {
+
+    }
+}
+
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorEnumExcludeAttribute : Attribute
+{
+    public byte[] ExcludedValues { get; private set; }
+
+    public SiegeEditorEnumExcludeAttribute(params byte[] excludedEnums)
+    {
+        ExcludedValues = excludedEnums;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorDropdownAttribute : Attribute
+{
+    public SiegeEditorDropdownAttribute()
+    {
+
+    }
+}
+
+// works only with UiTagInputFieldDisplay
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorCustomHeightAttribute : Attribute
+{
+    public float Height { get; private set; }
+
+    public SiegeEditorCustomHeightAttribute(float height)
+    {
+        Height = height;
+    }
+}
+
+// works only with UiTagInputFieldDisplay
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorCustomWidthAttribute : Attribute
+{
+    public float Width { get; private set; }
+
+    public SiegeEditorCustomWidthAttribute(float width)
+    {
+        Width = width;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorCustomTextTooltipAttribute : Attribute
+{
+    public string Content { get; private set; }
+    public bool IsTag { get; private set; }
+
+    public SiegeEditorCustomTextTooltipAttribute(string content, bool isTag = false)
+    {
+        Content = content;
+        IsTag = isTag;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SiegeEditorOnlyUnityEditor : Attribute
+{
+    public SiegeEditorOnlyUnityEditor() { }
+}
---
+++ UiValueEditorGenerator.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class UiValueEditorGenerator : MonoBehaviour
+{
+    public List<UiValueInputFieldEditorEdit> UiInputFieldPrefabs;
+    public List<UiValueSliderEditorEdit> UiSliderPrefabs;
+    public List<UiCheckboxEditorEdit> UiCheckboxPrefabs;
+    public List<UiTagInputFieldDisplay> UiTagInputFieldDisplayPrefabs;
+    public UiMultipleInputFieldEditorEdit UiMultipleInputFieldEditorEditPrefab;
+
+    public UiValueEditorEdit GenerateSingleUiField(object obj, string fieldName, Transform holder, Action onValueEdit = null, Func<string, bool> condition = null, ValueEditorEditType type = ValueEditorEditType.Default)
+    {
+        FieldInfo fieldInfo = obj.GetType().GetField(fieldName);
+        UiValueEditorEdit outputUi = GenerateSingleUiEdit(fieldInfo, holder, type);
+
+        outputUi.Init(obj, fieldInfo, onValueEdit, condition);
+        return outputUi;
+    }
+
+    private UiValueEditorEdit GenerateSingleUiEdit(FieldInfo fieldInfo, Transform holder, ValueEditorEditType type)
+    {
+        SiegeEditorSliderAttribute slider = Attribute.GetCustomAttribute(fieldInfo, typeof(SiegeEditorSliderAttribute)) as SiegeEditorSliderAttribute;
+        UiValueEditorEdit uiEdit = null;
+        Type fieldType = fieldInfo.FieldType;
+
+        if (fieldType == typeof(string))
+        {
+            uiEdit = Instantiate(GetTagInputFieldPrefab(type), holder);
+        }
+        else if (fieldType == typeof(bool))
+        {
+            uiEdit = Instantiate(GetCheckboxPrefab(type), holder);
+        }
+        else if (slider != null)
+        {
+            uiEdit = Instantiate(GetSliderPrefab(type), holder);
+        }
+        else
+        {
+            uiEdit = Instantiate(GetInputFieldPrefab(type), holder);
+        }
+
+        return uiEdit;
+    }
+
+    private UiValueEditorEdit GetInputFieldPrefab(ValueEditorEditType type)
+    {
+        UiValueEditorEdit result = UiInputFieldPrefabs.Find(x => x.PrefabType == type);
+        if(result == null)
+        {
+            return UiInputFieldPrefabs.Find(x => x.PrefabType == ValueEditorEditType.Default);
+        }
+        return result;
+    }
+
+    private UiValueEditorEdit GetSliderPrefab(ValueEditorEditType type)
+    {
+        UiValueEditorEdit result = UiSliderPrefabs.Find(x => x.PrefabType == type);
+        if (result == null)
+        {
+            return UiSliderPrefabs.Find(x => x.PrefabType == ValueEditorEditType.Default);
+        }
+        return result;
+    }
+
+    private UiValueEditorEdit GetCheckboxPrefab(ValueEditorEditType type)
+    {
+        UiValueEditorEdit result = UiCheckboxPrefabs.Find(x => x.PrefabType == type);
+        if (result == null)
+        {
+            return UiCheckboxPrefabs.Find(x => x.PrefabType == ValueEditorEditType.Default);
+        }
+        return result;
+    }
+
+    private UiValueEditorEdit GetTagInputFieldPrefab(ValueEditorEditType type)
+    {
+        UiValueEditorEdit result = UiTagInputFieldDisplayPrefabs.Find(x => x.PrefabType == type);
+        if (result == null)
+        {
+            return UiTagInputFieldDisplayPrefabs.Find(x => x.PrefabType == ValueEditorEditType.Default);
+        }
+        return result;
+    }
+
+    public UiMultipleInputFieldEditorEdit GenerateMultipleUiField(object obj, string[] fieldNames, string displayedName, Transform holder, Action onValueEdit = null, Func<string, bool> condition = null, ValueEditorEditType type = ValueEditorEditType.Default)
+    {
+        List<UiValueInputFieldEditorEdit> inputFields = new List<UiValueInputFieldEditorEdit>();
+
+        for (int i = 0; i < fieldNames.Length; i++)
+        {
+            FieldInfo fieldInfo = obj.GetType().GetField(fieldNames[i]);
+            UiValueEditorEdit outputUi = GenerateSingleUiEdit(fieldInfo, holder, type);
+            UiValueInputFieldEditorEdit uiInputField = outputUi as UiValueInputFieldEditorEdit;
+
+            if (uiInputField != null)
+            {
+                uiInputField.Init(obj, fieldInfo, onValueEdit, condition);
+                inputFields.Add(uiInputField);
+            }
+            else
+            {
+                Destroy(outputUi.gameObject);
+            }
+        }
+
+        UiMultipleInputFieldEditorEdit uiEdit = Instantiate(UiMultipleInputFieldEditorEditPrefab, holder);
+        uiEdit.Init(inputFields, displayedName);
+
+        return uiEdit;
+    }
+
+    public UiValueEditorEdit[] GenerateUiEdits(object obj, Transform holder, Action onValueEdit = null, Func<string, bool> condition = null, ValueEditorEditType type = ValueEditorEditType.Default, params string[] groups)
+    {
+        FieldInfo[] fields = obj.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+        UiValueEditorEdit[] output = new UiValueEditorEdit[fields.Length];
+        List<FieldInfo> finalFields = new List<FieldInfo>();
+
+        output = FilterFields(fields, out fields, groups);
+
+        for (int i = 0; i < fields.Length; i++)
+        {
+            FieldInfo field = fields[i];
+            SiegeEditorSliderAttribute slider = Attribute.GetCustomAttribute(field, typeof(SiegeEditorSliderAttribute)) as SiegeEditorSliderAttribute;
+            UiValueEditorEdit uiEdit = null;
+            Type fieldType = field.FieldType;
+
+            uiEdit = GenerateSingleUiEdit(field, holder, type);
+            output[i] = uiEdit;
+            uiEdit.Init(obj, field, onValueEdit, condition);
+        }
+
+        return output;
+    }
+
+    public void UpdateValueEditorEdits(object obj, UiValueEditorEdit[] uiEdits, params string[] groups)
+    {
+        FieldInfo[] fields = obj.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+        FilterFields(fields, out fields, groups);
+
+        for (int i = 0; i < fields.Length; i++)
+        {
+            FieldInfo field = fields[i];
+            uiEdits[i].UpdateUI(obj, field);
+        }
+    }
+
+    public void UpdateSingleValueEditorUi(object obj, string name, UiValueEditorEdit uiValueEditorEdit)
+    {
+        FieldInfo fieldInfo = obj.GetType().GetField(name);
+        uiValueEditorEdit.UpdateUI(obj, fieldInfo);
+    }
+
+    private UiValueEditorEdit[] FilterFields(FieldInfo[] fields, out FieldInfo[] output, string[] groups)
+    {
+        List<FieldInfo> finalFields = new List<FieldInfo>();
+        List<string> filters = groups.ToList();
+        for (int i = 0; i < fields.Length; i++)
+        {
+            FieldInfo field = fields[i];
+            SiegeEditorGroupAttribute group = Attribute.GetCustomAttribute(field, typeof(SiegeEditorGroupAttribute)) as SiegeEditorGroupAttribute;
+            bool exclude = Attribute.GetCustomAttribute(field, typeof(SiegeEditorExcludeAttribute)) as SiegeEditorExcludeAttribute != null;
+
+            if (!exclude)
+            {
+                if (groups == null || groups.Length == 0)
+                {
+                    finalFields.Add(field);
+                }
+                else
+                {
+                    if (group != null)
+                    {
+                        for (int j = 0; j < group.Groups.Length; j++)
+                        {
+                            if (filters.Contains(group.Groups[j]))
+                            {
+                                finalFields.Add(field);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        output = finalFields.ToArray();
+        return new UiValueEditorEdit[output.Length];
+    }
+}
